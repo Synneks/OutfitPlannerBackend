87f0b490321550f287a02806742c75fc3d4052a1
Synneks <Deniseneotescu@gmail.com>
2019-10-16 16:11:52 +0300

    Am mai structurat putin + gasit problema (dar nerezolvata, inca)

--- src/main/java/com/lid/outfitplannerbackend/persistence/UserRepository.java
+++ src/main/java/com/lid/outfitplannerbackend/persistence/UserRepository.java
@@ -4,10 +4,6 @@ import com.lid.outfitplannerbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
-
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-
 }
