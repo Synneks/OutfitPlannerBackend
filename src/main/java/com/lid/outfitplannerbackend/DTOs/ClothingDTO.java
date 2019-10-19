0628b7f74d1fa360cd6971987c854af45db48cb4
LiviaMaria21 <liviambordea@gmail.com>
2019-10-19 11:47:41 +0300

    bagat blob in baza de date

---
+++ src/main/java/com/lid/outfitplannerbackend/DTOs/ClothingDTO.java
@@ -0,0 +1,22 @@
+package com.lid.outfitplannerbackend.controllers;
+
+public class ClothingDTO {
+    private int id;
+    private String picture;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+}
