7205be5f096d830ff60dc5226c151be39f798fee
BlenderMan <ionutzispas619@gmail.com>
2019-12-18 12:24:44 +0200

    - sa ai spor, Denis

--- src/main/java/com/lid/outfitplannerbackend/services/OutfitService.java
+++ src/main/java/com/lid/outfitplannerbackend/services/OutfitService.java
@@ -1,21 +1,29 @@
 package com.lid.outfitplannerbackend.services;
 
+import com.lid.outfitplannerbackend.model.Clothing;
+import com.lid.outfitplannerbackend.model.Color;
 import com.lid.outfitplannerbackend.model.Outfit;
 import com.lid.outfitplannerbackend.persistence.OutfitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Component
 public class OutfitService implements IService<Outfit> {
 
     private final OutfitRepository outfitRepository;
+    private final ClothingService clothingService;
+    private final ColorService colorService;
 
     @Autowired
-    public OutfitService(OutfitRepository outfitRepository) {
+    public OutfitService(OutfitRepository outfitRepository, ClothingService clothingService, ColorService colorService) {
         this.outfitRepository = outfitRepository;
+        this.clothingService = clothingService;
+        this.colorService = colorService;
     }
 
     @Transactional
@@ -34,7 +42,22 @@ public class OutfitService implements IService<Outfit> {
         return outfitRepository.save(outfit);
     }
 
-    public Outfit update(Outfit outfit) {
-        return outfitRepository.save(outfit);
+    public Set<Outfit> generateOutfits(Clothing clothing) {
+        Set<Outfit> outfits = new HashSet<>();
+        List<Color> colors = clothing.getColors();
+        Color firstColor = null;
+        for (Color color : colors) {
+            if (!color.getName().equals("white") && !color.getName().equals("gray") && !color.getName().equals("black")) {
+                firstColor = color;
+            }
+        }
+        if (firstColor == null) {
+            firstColor = colors.get(0);
+        }
+        List<List<Color>> colorHarmonies = colorService.getAllMatchingCombinations(firstColor);
+        for (List<Color> colorHarmony : colorHarmonies) {
+
+        }
+        return outfits;
     }
 }
