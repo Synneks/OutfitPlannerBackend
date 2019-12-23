60d647919948aef92b10cdfc39ad0c5684de8458
LiviaMaria21 <liviambordea@gmail.com>
2019-12-23 21:06:34 +0200

    Modified Outfit Controller to return OutfitDTO

--- src/main/java/com/lid/outfitplannerbackend/controllers/OutfitController.java
+++ src/main/java/com/lid/outfitplannerbackend/controllers/OutfitController.java
@@ -1,6 +1,11 @@
 package com.lid.outfitplannerbackend.controllers;
 
+import com.lid.outfitplannerbackend.DTOs.ClothingDtoMapper;
+import com.lid.outfitplannerbackend.DTOs.OutfitDTO;
+import com.lid.outfitplannerbackend.DTOs.OutfitDtoMapper;
+import com.lid.outfitplannerbackend.model.Clothing;
 import com.lid.outfitplannerbackend.model.Outfit;
+import com.lid.outfitplannerbackend.services.ClothingService;
 import com.lid.outfitplannerbackend.services.OutfitService;
 import com.lid.outfitplannerbackend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +13,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @CrossOrigin
 @RestController
@@ -17,6 +24,7 @@ public class OutfitController {
     private final OutfitService outfitService;
 
     private final UserService userService;
+
 
     @Autowired
     public OutfitController(OutfitService outfitService, UserService userService) {
@@ -27,8 +35,9 @@ public class OutfitController {
     @GetMapping(value = "users/{userId}/outfits")
     public ResponseEntity getAll(@PathVariable int userId) {
         List<Outfit> outfits = userService.getById(userId).getOutfits();
-        if (!outfits.isEmpty()) {
-            return new ResponseEntity<>(outfits, HttpStatus.OK);
+        List<OutfitDTO> outfitDTOs = outfits.stream().map(OutfitDtoMapper::entityToDto).collect(Collectors.toList());
+        if (!outfitDTOs.isEmpty()) {
+            return new ResponseEntity<>(outfitDTOs, HttpStatus.OK);
         } else {
             return new ResponseEntity<>("No outfits found!", HttpStatus.NOT_FOUND);
         }
@@ -38,7 +47,7 @@ public class OutfitController {
     public ResponseEntity getById(@PathVariable int id, @PathVariable int userId) {
         Outfit outfit = outfitService.getById(id);
         if (outfit != null) {
-            return new ResponseEntity<>(outfit, HttpStatus.OK);
+            return new ResponseEntity<>(OutfitDtoMapper.entityToDto(outfit), HttpStatus.OK);
         } else {
             return new ResponseEntity<>("Outfit not found!", HttpStatus.NOT_FOUND);
         }
@@ -49,9 +58,11 @@ public class OutfitController {
         try {
             outfit = outfitService.insert(outfit);
             outfit = userService.insertOutfit(userId, outfit);
-            return new ResponseEntity<>(outfit, HttpStatus.OK);
+            return new ResponseEntity<>(OutfitDtoMapper.entityToDto(outfit), HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>("Outfit could not be added to user!", HttpStatus.NOT_FOUND);
         }
     }
+
+
 }
